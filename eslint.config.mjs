eec954b549927f01dc77855d77048752a1976b47
morz-mamun <morzmamun@gmail.com>
2026-03-16 01:47:07 +0600

    chore: implement huskey

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,27 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended", ...tseslint.configs.recommended],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "no-undef": "warn",
+      "prefer-const": "error",
+      "no-console": "warn",
+    },
+  },
+  {
+    ignores: [
+      "node_modules",
+      "dist",
+      "eslint.config.mjs",
+      "commitlint.config.mjs",
+    ],
+  },
 ]);
+
